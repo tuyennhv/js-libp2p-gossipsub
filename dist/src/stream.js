9109f77ee4b1c33407092aa9e87f8bfb7af61012
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-08-15 11:46:32 +0700

    feat: add dist

---
+++ dist/src/stream.js
@@ -0,0 +1,50 @@
+import { abortableSource } from 'abortable-iterator';
+import { pipe } from 'it-pipe';
+import { pushable } from 'it-pushable';
+import { encode, decode } from 'it-length-prefixed';
+export class OutboundStream {
+    rawStream;
+    pushable;
+    closeController;
+    maxBufferSize;
+    constructor(rawStream, errCallback, opts) {
+        this.rawStream = rawStream;
+        this.pushable = pushable({ objectMode: false });
+        this.closeController = new AbortController();
+        this.maxBufferSize = opts.maxBufferSize ?? Infinity;
+        pipe(abortableSource(this.pushable, this.closeController.signal, { returnOnAbort: true }), (source) => encode(source), this.rawStream).catch(errCallback);
+    }
+    get protocol() {
+        // TODO remove this non-nullish assertion after https://github.com/libp2p/js-libp2p-interfaces/pull/265 is incorporated
+        return this.rawStream.protocol;
+    }
+    push(data) {
+        if (this.pushable.readableLength > this.maxBufferSize) {
+            throw Error(`OutboundStream buffer full, size > ${this.maxBufferSize}`);
+        }
+        this.pushable.push(data);
+    }
+    close() {
+        this.closeController.abort();
+        // similar to pushable.end() but clear the internal buffer
+        this.pushable.return();
+        this.rawStream.close();
+    }
+}
+export class InboundStream {
+    source;
+    rawStream;
+    closeController;
+    constructor(rawStream, opts = {}) {
+        this.rawStream = rawStream;
+        this.closeController = new AbortController();
+        this.source = abortableSource(pipe(this.rawStream, (source) => decode(source, opts)), this.closeController.signal, {
+            returnOnAbort: true
+        });
+    }
+    close() {
+        this.closeController.abort();
+        this.rawStream.close();
+    }
+}
+//# sourceMappingURL=stream.js.map
